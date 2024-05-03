9a78d315de846997bce61d710944bc27f3888238
DanielVillegasJ <163043315+DanielVillegasJ@users.noreply.github.com>
2024-05-03 18:53:50 -0500

    Delete sazonla80 Versión Antigua directory

--- sazonla80 Versión Antigua/log_in/salir.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-
-echo "<script>window.location='../log_in/index.html';</script>";
-?>
--- sazonla80 Versión Antigua/sign_in_clientes/conexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost","root","","sazonla80")or die("sin conexion");
-?>
